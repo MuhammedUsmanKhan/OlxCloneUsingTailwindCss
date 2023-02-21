2f08f287984cf0cbaa2db6b1aa0905adb1a98e9d
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-02-21 15:58:42 +0500

    Before Card Almost Done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/*.html"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: '0.9rem',
+      }
+    },
+  },
+  plugins: [],
+}
