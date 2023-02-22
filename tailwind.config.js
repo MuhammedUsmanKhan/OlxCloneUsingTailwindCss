9f569d39c94fdee2a50aec7535788817eda01571
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-02-22 13:54:16 +0500

    Navbar Almost Done :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     extend: {
       fontSize: {
         sm: '0.9rem',
+      },
+      screens: {
+        // 'tablet': '640px',
+        // // => @media (min-width: 640px) { ... }
+  
+        // 'laptop': '1024px',
+        // // => @media (min-width: 1024px) { ... }
+          // 'md':'769px',
+        // 'desktop': '1280px',
+        // // => @media (min-width: 1280px) { ... }
+      },
+      backgroundImage: {
+        'borderimage': "url('/main/images/iconSellBorder_noinline.d9eebe038fbfae9f90fd61d971037e02.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
