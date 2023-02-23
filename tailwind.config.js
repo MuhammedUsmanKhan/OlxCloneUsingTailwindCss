f8969a40359a7bc0399b254a1c1f5cb372e4069b
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-02-23 19:17:12 +0500

    Mobile Layout Done Alhamdulillah :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,16 @@ module.exports = {
       fontSize: {
         sm: '0.9rem',
       },
-      screens: {
-        // 'tablet': '640px',
-        // // => @media (min-width: 640px) { ... }
-  
-        // 'laptop': '1024px',
-        // // => @media (min-width: 1024px) { ... }
-          // 'md':'769px',
-        // 'desktop': '1280px',
-        // // => @media (min-width: 1280px) { ... }
+      fontFamily: {
+        'serif': ['ui-serif', 'Georgia'],
+        // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
+        // 'display': ['Oswald', ...],
+        // 'body': ['"Open Sans"', ...],
       },
       backgroundImage: {
         'borderimage': "url('/main/images/iconSellBorder_noinline.d9eebe038fbfae9f90fd61d971037e02.svg')",
         'PlusImage': "url('/main/images/iconPlusSell_noinline.75fc7ea23e80b50447cf5757d8ef083a.svg')",
+        'logoOfGreatersymbol':"url('/main/images/greatersignicon.svg')",
       }
     },
   },
