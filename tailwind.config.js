56d118c235376e635cf1a585fff095d818ac8388
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-02-24 16:51:00 +0500

    Almost Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         'borderimage': "url('/main/images/iconSellBorder_noinline.d9eebe038fbfae9f90fd61d971037e02.svg')",
         'PlusImage': "url('/main/images/iconPlusSell_noinline.75fc7ea23e80b50447cf5757d8ef083a.svg')",
         'logoOfGreatersymbol':"url('/main/images/greatersignicon.svg')",
+        'BacktoTopicon':"url('/main/images/backToTopIcon_noinline.7b82f4f094ade4510834eaf20d4ce5ab.svg')",
       }
     },
   },
